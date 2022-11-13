546ee7c1945c345b2699949793d6b8a37d5a4386
UnaxLabak <unaxlabak2@gmail.com>
2022-11-13 13:13:05 +0100

    Factory creado

---
+++ src/main/java/businesslogic/BLFacadeFactory.java
@@ -0,0 +1,49 @@
+package businesslogic;
+
+import java.awt.Color;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+
+import javax.swing.UIManager;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import businesslogic.BLFacade;
+import businesslogic.BLFacadeImplementation;
+import configuration.ConfigXML;
+import dataAccess.DataAccess;
+import gui.MainGUI;
+
+public class BLFacadeFactory {
+
+	public BLFacadeFactory() {
+		
+	}
+	public  BLFacade createBLFacade(ConfigXML c) throws MalformedURLException {
+
+		
+			BLFacade  appFacadeInterface;
+
+			if (c.isBusinessLogicLocal()) {
+				DataAccess da= new DataAccess(c.getDataBaseOpenMode().equals("initialize"));
+				return new BLFacadeImplementation(da); 
+			}else{
+				String serviceName= "http://"+c.getBusinessLogicNode() +":"+ c.getBusinessLogicPort()+"/ws/"+c.getBusinessLogicName()+"?wsdl";
+				 
+				//URL url = new URL("http://localhost:9999/ws/ruralHouses?wsdl");
+				URL url = new URL(serviceName);
+
+		 
+		        //1st argument refers to wsdl document above
+				//2nd argument is service name, refer to wsdl document above
+//		        QName qname = new QName("http://businessLogic/", "FacadeImplementationWSService");
+		        QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
+		 
+		        Service service = Service.create(url, qname);
+
+		         appFacadeInterface = service.getPort(BLFacade.class);	
+		         return appFacadeInterface;
+			}
+	}	 
+}
--- src/main/java/gui/ApplicationLauncher.java
+++ src/main/java/gui/ApplicationLauncher.java
@@ -1,17 +1,15 @@
 package gui;
 
 import java.awt.Color;
-import java.net.URL;
+
 import java.util.Locale;
 
-import javax.swing.UIManager;
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
 
-import businesslogic.BLFacade;
-import businesslogic.BLFacadeImplementation;
+
+import businesslogic.BLFacadeFactory;
+
 import configuration.ConfigXML;
-import dataAccess.DataAccess;
+
 
 public class ApplicationLauncher { 
 	
@@ -29,51 +27,14 @@ public class ApplicationLauncher {
 		
 		MainGUI a=new MainGUI();
 		a.setVisible(true);
+		
+		
 
 
 		try {
-			
-			BLFacade appFacadeInterface;
-//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
-//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-			
-			if (c.isBusinessLogicLocal()) {
-				
-				//In this option the DataAccess is created by FacadeImplementationWS
-				//appFacadeInterface=new BLFacadeImplementation();
+			BLFacadeFactory blFacade = new BLFacadeFactory();
 
-				//In this option, you can parameterize the DataAccess (e.g. a Mock DataAccess object)
-
-				DataAccess da= new DataAccess(c.getDataBaseOpenMode().equals("initialize"));
-				appFacadeInterface=new BLFacadeImplementation(da);
-
-				
-			}
-			
-			else { //If remote
-				
-				 String serviceName= "http://"+c.getBusinessLogicNode() +":"+ c.getBusinessLogicPort()+"/ws/"+c.getBusinessLogicName()+"?wsdl";
-				 
-				//URL url = new URL("http://localhost:9999/ws/ruralHouses?wsdl");
-				URL url = new URL(serviceName);
-
-		 
-		        //1st argument refers to wsdl document above
-				//2nd argument is service name, refer to wsdl document above
-//		        QName qname = new QName("http://businessLogic/", "FacadeImplementationWSService");
-		        QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
-		 
-		        Service service = Service.create(url, qname);
-
-		         appFacadeInterface = service.getPort(BLFacade.class);
-			} 
-			/*if (c.getDataBaseOpenMode().equals("initialize")) 
-				appFacadeInterface.initializeBD();
-				*/
-			MainGUI.setBussinessLogic(appFacadeInterface);
-
-		
+			MainGUI.setBussinessLogic(blFacade.createBLFacade(c));
 
 			
 		}catch (Exception e) {
