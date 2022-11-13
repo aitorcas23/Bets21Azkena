f5b7b5cb647c0c3d5f886506394bf05292913bce
aitorcas23 <aitorcas23@gmail.com>
2022-11-13 23:55:15 +0100

    intefaz y eso

--- src/main/java/domain/ExtendedEventIterator.java
+++ src/main/java/domain/ExtendedEventIterator.java
@@ -1,57 +1,83 @@
 package domain;
 
 import java.util.Iterator;
-import java.util.List;
+import java.util.Vector;
 
-public class ExtendedEventIterator<Event> implements Iterator<Event> {
+public class ExtendedEventIterator implements ExtendedIterator<Event> {
     private int pos;
-    private List<Event> array;
+    private Vector<Event> array;
     
-    public ExtendedEventIterator(List<Event> array) {
+    public ExtendedEventIterator() {
+    	this.pos =0;
+    	this.array = new Vector<Event>();
+    }
+    
+    public ExtendedEventIterator(Iterable<Event> array) {
     	this.pos = 0;
-    	this.array = array;
+    	this.array = new Vector<Event>();
+    	for(Event e : array) {
+    		this.array.add(e);
+    	}
     }
 	
 	@Override
 	public boolean hasNext() {
+		return (pos >= 0 && pos < array.size());
+		
+		/*
 		if(pos >array.size()-1) {
 			return true;
 		} else {
 			return false;
-		}
+		}*/
 	}
 
 	@Override
 	public Event next() {
-		try {
-			return array.get(pos+1);
-		} catch (Exception ex) {
-			return null;
-		}
+		Event e = array.get(pos);
+		pos++;
+		return e;
 	}
 	
+	@Override
 	public boolean hasPrevious() {
-		if (pos-1>0) {
-			return true;
-		} 
-		else {
-			return false;
-		}
+		return (pos >= 0 && pos < array.size());
 	}
 
+	@Override
 	public Event previous() {
-		try {
-			return array.get(pos-1);
-		} catch (Exception ex) {
-			return null;
-		}
+		Event e = array.get(pos);
+		pos--;
+		return e;
 	}
 	
+	@Override
 	public void goFirst() {
 		pos = 0;
 	}
 	
+	@Override
 	public void goLast() {
 		pos = array.size()-1;
+	}
+
+	@Override
+	public void add(Event ev) {
+		array.add(ev);
+	}
+
+	@Override
+	public Iterator iterator() {
+		return this;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return array.isEmpty();
+	}
+	
+	@Override
+	public int size() {
+		return array.size();
 	}
 }
---
+++ src/main/java/domain/ExtendedIterator.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public interface ExtendedIterator<Event> extends Iterator<Event>, Iterable{
+	//uneko elementua itzultzen du eta aurrekora pasatzen da
+	public Event previous();
+	//true aurreko elementua existitzen bada.
+	public boolean hasPrevious();
+	//Lehendabiziko elementuan kokatzen da.
+	public void goFirst();
+	//Azkeneko elementuan kokatzen da.
+	public void goLast();
+	
+	void add(Event ev);
+	public boolean isEmpty();
+	public int size();
+}
